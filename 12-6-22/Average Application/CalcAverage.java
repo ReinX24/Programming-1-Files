accc83650de0f40746af50fcd3ac1eccacfe328f
Rein <reinX244@gmail.com>
2022-12-09 21:53:47 +0800

    Added try catch blocks to handle exceptions Try catch blocks have been added to areas in the code where exceptions may occur. Most caught exceptions are InputMismatchException exceptions.
    Also made the menus look cleaner by adding serparators for different
    parts of these menus.

--- 12-6-22/Average Application/CalcAverage.java
+++ 12-6-22/Average Application/CalcAverage.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CalcAverage {
@@ -6,40 +8,55 @@ public class CalcAverage {
     Scanner scanOne = new Scanner(System.in); // Scanner that will accept user input
     ArrayList<Float> numArr = new ArrayList<Float>(); // ArrayList where values will be stored
     int userChoice = 0; // stores userChoice
-    Float numUserAdd = 0.0f;
+    Float numUserAdd = null;
 
     public void mainMenu() {
 
+        userChoice = 0; // resets user choice to avoid bugs, an example is when it takes the
+        // last input as userChoice as a decision when an exception is supposed to occur
+        System.out.println("====================");
         System.out.println("[Average Calculator]");
-        System.out.println("[1] Add Number to Array");
+        System.out.println("====================");
+        System.out.println("[1] Add No. to Array");
         System.out.println("[2] Clear Array");
         System.out.println("[3] Show Array");
         System.out.println("[4] Exit Application");
-        System.out.print("Input: ");
+        System.out.println("====================");
 
-        userChoice = scanOne.nextInt();
+        try {
+
+            System.out.print("Input: ");
+            userChoice = scanOne.nextInt();
+
+        }
+
+        catch (InputMismatchException ex) {
+
+            scanOne.nextLine();
+
+        }
 
         switch (userChoice) {
 
             case 1:
-            addNumMenu();
-            break;
+                addNumMenu();
+                break;
 
             case 2:
-            // method that resets array
-            break;
+                // method that resets ArrayList
+                break;
 
             case 3:
-            showArr();
-            break;
+                showArr();
+                break;
 
             case 4:
-            exitApp(); // exits application
-            break;
+                exitApp(); // exits application
+                break;
 
             default:
-            mainMenu(); // returns to main menu
-            break;
+                mainMenu(); // returns to main menu
+                break;
 
         }
 
@@ -47,28 +64,77 @@ public class CalcAverage {
 
     public void addNumMenu() {
 
-        System.out.println("[1] Add a Number to Array");
+        userChoice = 0;
+        System.out.println("====================");
+        System.out.println("[Add Number to Array]");
+        System.out.println("[1] Add a Number");
         System.out.println("[2] Main Menu");
-        System.out.print("Input: ");
-        userChoice = scanOne.nextInt();
+        System.out.println("====================");
 
-        if (userChoice == 1) {
+        try {
 
-            System.out.print("Input number: ");
-            numUserAdd = scanOne.nextFloat();
-            addNum(numUserAdd);
+            System.out.print("Input: ");
+            userChoice = scanOne.nextInt();
 
         }
 
-        else {
+        catch (InputMismatchException ex) {
 
-            mainMenu(); // returns to main menu
+            scanOne.nextLine();
+
+        }
+
+        switch (userChoice) {
+
+            case 1:
+                addNumChoice(); // goes to a menu where the user inputs a number
+                break;
+
+            case 2:
+                mainMenu(); // returns to main menu
+                break;
+
+            default:
+                errorReset(); // says an error message an returns to main menu
+                break;
 
         }
 
     }
 
-    public void addNum(Float paramNum) {
+    public void addNumChoice() {
+
+        userChoice = 0;
+        System.out.println("====================");
+        System.out.println("[Insert No. to Add]");
+
+        try {
+
+            System.out.print("Input number: ");
+            Double numHolder = scanOne.nextDouble();
+
+            numUserAdd = numHolder.floatValue();
+
+        if (numUserAdd == numHolder.floatValue()) { // adds number to ArrayList when it is a number
+
+            addNum(numUserAdd);
+            System.out.println("Number added!"); // tells user their number has been added
+            addNumMenu(); // return to menu before
+
+        }
+
+        }
+
+        catch (InputMismatchException ex) {
+
+            scanOne.nextLine();
+            errorReset();
+
+        }
+
+    }
+
+    public void addNum(Float paramNum) { // method that adds inputted number to the ArrayList
 
         numArr.add(paramNum);
 
@@ -76,15 +142,75 @@ public class CalcAverage {
 
     public void showArr() {
 
+        userChoice = 0;
+        System.out.println("====================");
+        System.out.println("Array List Contents:");
         System.out.println(numArr);
+        System.out.println("====================");
+        System.out.println("[1] Sort Array");
+        System.out.println("[2] Return to Main Menu");
+        System.out.println("====================");
+
+        try {
+
+            System.out.print("Input: ");
+            userChoice = scanOne.nextInt();
+
+        }
+
+        catch (InputMismatchException ex) {
+
+            scanOne.nextLine();
+
+        }
+
+        switch (userChoice) {
+
+            case 1:
+
+                try {
+
+                    Collections.sort(numArr); // sorts the array
+                    System.out.println("====================");
+                    System.out.println("Sorting array...");
+                    Thread.sleep(3000); // delays printing next line
+                    System.out.println("Array sorted!");
+                    System.out.println("====================");
+                    showArr(); // shows Array List but sorted this time
+                }
+
+                catch (InterruptedException ex) {
+
+                    ex.printStackTrace();
+
+                }
+
+                break;
+
+            case 2:
+                mainMenu();
+                break;
+
+            default:
+                errorReset();
+                break;
+
+        }
+
+    }
+
+    public void errorReset() {
+
+        System.out.println("[Error Occured, Returning to Main Menu]");
+        mainMenu(); // return to main menu
 
     }
 
     public void exitApp() {
 
-        System.out.println("Exited Application");
+        System.out.println("[Exited Application]");
         System.exit(0);
 
     }
-    
+
 }
--- 12-6-22/Average Application/MainApp.java
+++ 12-6-22/Average Application/MainApp.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * Author: Rein Aldwin E. Solis
+ * Programming Language: Java
+ * Creation Date: 12/9/22
+ * School: Universidad De Dagupan
+ * Description: Calculator that finds the average of an ArrayList of numbers
+ * 
+ */
+
 class MainApp {
 
     public static void main(String[] args) {
+
+        /*
+         * NOTE: This application (if you can even call it that) uses object oriented programming concepts,
+         * a subject or topic that we will be tacking in our second year (as stated in curriculum). If you
+         * do not understand the code below and in the other file, do not worry for this will eventually be
+         * taught in our 2nd year.
+         * 
+         */
 
         /*
          * 12/9/22
@@ -18,6 +38,19 @@ class MainApp {
         aveOne.mainMenu();
 
         }
+
+        // ArrayList<Float> floatArr = new ArrayList<Float>();
+
+        // Scanner scanOne = new Scanner(System.in);
+        // Float fOne = null;
+
+        // System.out.println("Insert Element");
+        // fOne = scanOne.nextFloat();
+        // floatArr.add(fOne);
+        // System.out.println(floatArr);
+        
+        
+
 
     }
 

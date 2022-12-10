0c1472e52fafffde48dbedb3a8e8a1acaa63a814
Rein <reinX244@gmail.com>
2022-12-10 19:47:31 +0800

    Finding mode of our elements development started A feature that finds the mode of our elements has started development, the feature still has some bugs to fix before it could be fully functional.

--- 12-6-22/Average Application/CalcAverage.java
+++ 12-6-22/Average Application/CalcAverage.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.InputMismatchException;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 public class CalcAverage {
@@ -22,7 +23,8 @@ public class CalcAverage {
         System.out.println("[3] Show|Sort Array");
         System.out.println("[4] Average of Array");
         System.out.println("[5] Median of Array");
-        System.out.println("[6] Exit Application");
+        System.out.println("[6] Mode of Array");
+        System.out.println("[7] Exit Application");
         System.out.println("====================");
 
         try {
@@ -61,6 +63,9 @@ public class CalcAverage {
                 break;
 
             case 6:
+                modeMenu(); // method that goes to mode menu
+
+            case 7:
                 exitApp(); // exits application
                 break;
 
@@ -352,15 +357,28 @@ public class CalcAverage {
 
             }
 
-            arrAverage = sumOfNum / numArr.size(); // calculates for average
+            if (numArr.size() > 1) {
 
-            System.out.println("====================");
-            System.out.println("Adding numbers...");
-            Thread.sleep(1000); // delays printing next line, 1 second
-            System.out.println("Finding average...");
-            Thread.sleep(1000); // delays printing next line, 1 second
-            System.out.println("Average found!");
-            System.out.println("Average: " + arrAverage);
+                arrAverage = sumOfNum / numArr.size(); // calculates for average
+
+                System.out.println("====================");
+                System.out.println("Adding numbers...");
+                Thread.sleep(1000); // delays printing next line, 1 second
+                System.out.println("Finding average...");
+                Thread.sleep(1000); // delays printing next line, 1 second
+                System.out.println("Average found!");
+                System.out.println("Average: " + arrAverage);
+
+            }
+
+            else {
+
+                System.out.println("No average found!");
+                System.out.println("[Need more numbers]");
+
+            }
+
+            mainMenu(); // return to main menu
         }
 
         catch (InterruptedException ex) {
@@ -424,39 +442,199 @@ public class CalcAverage {
 
         try {
 
-        if (numArr.size() % 2 != 0) { // odd number size
+            if (numArr.size() > 1) {
 
-            ArrayList<Float> arrClone = new ArrayList<Float>(numArr); // clones our array list to another array list
-            Collections.sort(arrClone); // sorts cloned array list
-            Float medianFloat = 0.0f; // stores median
-            System.out.println("Finding median...");
-            Thread.sleep(1000); // 1 second delay
-            medianFloat = arrClone.get((arrClone.size() - 1) / 2); 
-            System.out.println("Median found!");
-            System.out.println("Median: " + medianFloat);
+                if (numArr.size() % 2 != 0) { // odd number size
+
+                    ArrayList<Float> arrClone = new ArrayList<Float>(numArr); // clones our array list to another array
+                                                                              // list
+                    Collections.sort(arrClone); // sorts cloned array list
+                    Float medianFloat = 0.0f; // stores median
+                    System.out.println("Finding median...");
+                    Thread.sleep(1000); // 1 second delay
+                    medianFloat = arrClone.get((arrClone.size() - 1) / 2);
+                    System.out.println("Median found!");
+                    System.out.println("Median: " + medianFloat);
+
+                }
+
+                else { // even number size
+
+                    ArrayList<Float> arrClone = new ArrayList<Float>(numArr); // clones our array list to another array
+                                                                              // list
+                    Collections.sort(arrClone); // sorts cloned array list
+                    Float medianFloat = 0.0f; // stores median
+                    System.out.println("Finding median...");
+                    Thread.sleep(1000); // 1 second delay
+                    medianFloat = (arrClone.get(arrClone.size() / 2) + arrClone.get(arrClone.size() / 2 - 1)) / 2;
+                    System.out.println("Median found!");
+                    System.out.println("Median: " + medianFloat);
+
+                }
+
+            }
+
+            else {
+
+                System.out.println("No median found!");
+                System.out.println("[Need more numbers]");
+
+            }
+
+            mainMenu(); // return to main menu
 
         }
-
-        else { // even number size
-
-            ArrayList<Float> arrClone = new ArrayList<Float>(numArr); // clones our array list to another array list
-            Collections.sort(arrClone); // sorts cloned array list
-            Float medianFloat = 0.0f; // stores median
-            System.out.println("Finding median...");
-            Thread.sleep(1000); // 1 second delay
-            medianFloat = (arrClone.get(arrClone.size() / 2) + arrClone.get(arrClone.size() / 2 - 1)) / 2; 
-            System.out.println("Median found!");
-            System.out.println("Median: " + medianFloat);
-
-        }
-
-    }
 
         catch (InterruptedException ex) {
 
             errorReset();
 
         }
+
+    }
+
+    public void modeMenu() {
+
+        userChoice = 0;
+        System.out.println("====================");
+        System.out.println("Array List Contents:");
+        System.out.println(numArr);
+        System.out.println("====================");
+        System.out.println("[Array List Mode]");
+        System.out.println("[1] Find Mode");
+        System.out.println("[2] Description");
+        System.out.println("[3] Main Menu");
+        System.out.println("====================");
+
+        try {
+
+            System.out.print("Input: ");
+            userChoice = scanOne.nextInt();
+
+        }
+
+        catch (InputMismatchException ex) {
+
+            scanOne.nextLine();
+
+        }
+
+        switch (userChoice) {
+
+            case 1:
+                calcMode(); // method that calculates or finds median
+                break;
+
+            case 2:
+                modeDesc(); // method that shows description of finding moe
+                break;
+
+            case 3:
+                mainMenu();
+                break;
+
+            default:
+                errorReset();
+                break;
+
+        }
+
+    }
+
+    // TODO: Fix mode calculator
+    public void calcMode() { // calculates for mode
+
+            try {
+
+                if (numArr.size() > 1) {
+
+                ArrayList<Float> arrClone = new ArrayList<Float>(numArr); // clones original array list
+                Collections.sort(arrClone); // sorts the array
+                LinkedHashSet<Float> modeSet = new LinkedHashSet<Float>(); // LinkedHashSet holds our mode/s
+                // LinkedHashSet because this type of Set merges any duplicates in its Set
+                System.out.println("Finding mode/s...");
+                Thread.sleep(1000); // 1 second delay
+
+                // Finding our mode
+
+                Float modeNum = 0.0f; // stores indiv median to add to modeSet
+                int maxCount = 0; // max count of our mode
+
+                for (int i = 0; i < arrClone.size(); i++) { // loops through list
+
+                    int numCounter = 0; // stores how many times a number is similar to another number
+
+                    for (int j = 0; j < arrClone.size(); j++) { // another loop that goes through our list
+
+                        if (arrClone.get(i) == arrClone.get(j)) {
+                            // if the iterated number is similar to the second iterated number, increment
+                            // numCounter by one
+                            ++numCounter;
+
+                        }
+
+                    }
+
+                    // After inner for loop is broken, check if he current value is more frequent
+                    // than any
+                    // of the previous values
+
+                    if (numCounter > maxCount && numCounter >= 2) { // mode must occur 2 or more times
+
+                        modeNum = arrClone.get(i); // assigns current number to be the mode
+                        maxCount = numCounter; // counter of this number replaces old max count
+                        modeSet.removeAll(modeSet); // removes all past element since new number replaces these elements
+                        modeSet.add(modeNum); // adds current found mode
+
+                    }
+
+                    // If the current value has an equal count to our last
+
+                    else if (numCounter == maxCount && numCounter >= 2) {
+
+                        modeNum = arrClone.get(i);
+                        modeSet.add(modeNum);
+
+                    }
+
+
+                } // end of outer for loop
+
+                System.out.println("Mode/s found!");
+                System.out.println("Mode/s: " + modeSet);
+
+                // if (modeSet.size() > 0) { // if there are any modes, print elements
+
+                //     System.out.println("Mode/s found!");
+                //     System.out.println("Mode/s: " + modeSet);
+
+                // }
+
+                // else {
+
+                //     System.out.println("No mode found!");
+                //     System.out.println("[None of the elements repeat]");
+        
+                // }
+
+            }
+
+            else {
+
+                System.out.println("No mode found!");
+                System.out.println("[Need more numbers]");
+    
+            }
+
+            mainMenu(); // returns to main menu after all operations executed
+
+        }
+
+            catch (InterruptedException ex) {
+
+                errorReset();
+
+            }
 
     }
 
@@ -663,6 +841,49 @@ public class CalcAverage {
 
             case 2:
                 medianMenu(); // returns to median menu
+                break;
+
+            case 3:
+                mainMenu(); // returns to main menu
+                break;
+
+            default:
+                errorReset();
+                break;
+
+        }
+
+    }
+
+    public void modeDesc() { // description for mode
+
+        userChoice = 0;
+        System.out.println("====================");
+        System.out.println("Description: ");
+        System.out.println("> Finds the mode of");
+        System.out.println("our number set.");
+        System.out.println("====================");
+        System.out.println("[2] Back");
+        System.out.println("[3] Main Menu");
+        System.out.println("====================");
+
+        try {
+
+            System.out.print("Input: ");
+            userChoice = scanOne.nextInt();
+
+        }
+
+        catch (InputMismatchException ex) {
+
+            scanOne.nextLine();
+
+        }
+
+        switch (userChoice) {
+
+            case 2:
+                modeMenu(); // returns to mode menu
                 break;
 
             case 3:

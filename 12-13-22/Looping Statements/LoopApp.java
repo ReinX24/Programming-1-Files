9e1574521acc2c554516c20743a8c39a8f33f9f3
Rein <reinX244@gmail.com>
2022-12-13 15:13:01 +0800

    Looping and Iterating Statements Folder made Made a folder that will hold our demonstrations and exercises regarding looping.

---
+++ 12-13-22/Looping Statements/LoopApp.java
@@ -0,0 +1,30 @@
+/*
+ * Author: Rein Aldwin E. Solis
+ * Programming Language: Java
+ * Creation Date: 12/13/22
+ * School: Universidad De Dagupan
+ * Description: Looping or Iterating Statements
+ * 
+ */
+
+class LoopApp {
+
+    public static void main(String[] args) {
+
+        /*
+         * Looping or Iterating Statements - a type of statement that runs code for a
+         * finite or infinite number of times.
+         * 
+         * Different kinds of looping statements:
+         * - for
+         * - while
+         * - do while
+         * - for each
+         */
+
+         // for loops
+         
+
+    }
+
+}

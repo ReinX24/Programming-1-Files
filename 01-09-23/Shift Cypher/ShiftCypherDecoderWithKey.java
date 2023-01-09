635b1b5cbcc6e32f1ffe1756495839986b19781b
Rein <reinX244@gmail.com>
2023-01-09 17:36:21 +0800

    Added Shift Cypher Decoder With Key

---
+++ 01-09-23/Shift Cypher/ShiftCypherDecoderWithKey.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ShiftCypherDecoderWithKey {
+
+    public static void main(String[] args) {
+
+        Scanner scanOne = new Scanner(System.in);
+        System.out.println("[Shift Cypher Decoder With Key]");
+        System.out.print("Input encrypted message: ");
+        String encryptionString = scanOne.nextLine();
+        System.out.print("Input encryption key: ");
+        int encryptionKey = scanOne.nextInt();
+
+        String lowerEncryptionString = encryptionString.toLowerCase();
+        String plainText = "";
+
+        for (int i = 0; i < lowerEncryptionString.length(); i++) {
+            int charIndex = lowerEncryptionString.charAt(i);
+            charIndex -= 'a';
+            charIndex -= encryptionKey;
+
+            if (charIndex < 0) {
+                charIndex += 26;
+            }
+
+            char charConverted = (char) (charIndex + 'a');
+            plainText += charConverted;
+        }
+
+        plainText = plainText.toUpperCase();
+        System.out.println("Encrypted message: " + encryptionString);
+        System.out.println("Decrypted message: " + plainText);
+        scanOne.close();
+
+    }
+
+}

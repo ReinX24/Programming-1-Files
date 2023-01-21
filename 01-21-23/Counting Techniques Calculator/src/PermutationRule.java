3c792dd491867611c4b6aeddde8e89e03586abf9
Rein <reinX244@gmail.com>
2023-01-21 16:13:35 +0800

    PermutationRule code added

--- 01-21-23/Counting Techniques Calculator/src/PermutationRule.java
+++ 01-21-23/Counting Techniques Calculator/src/PermutationRule.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 public class PermutationRule {
 
     public static void main(String[] args) {
@@ -11,9 +13,27 @@ public class PermutationRule {
         // In this program, we are calculating for the number of [arrangements].
 
         String passWord = "ABCDE";
-        int passWordLength = passWord.length();
-        
+        int r = passWord.length();
+        int n = 26;
 
+        /* Formula for Permutation Rule: n! / (n - r)! */
+        BigInteger possibleArrangements = (factorialNum(n).divide(factorialNum(n - r)));
+        // using BigInteger since the result is a large number
+
+        /* Printing Results */
+        System.out.println("There are " + possibleArrangements + " such ordered arrangements.");
+        System.out.println("Hence, the probability required is: 1 / " + possibleArrangements);
+
+    }
+
+    /* Method that finds the factorial of a number through recursion */
+    public static BigInteger factorialNum(int paraNum) {
+        BigInteger bigIntParam = BigInteger.valueOf(paraNum); // converts paraNum to BigInteger
+        if (bigIntParam.equals(BigInteger.valueOf(0))) { // base case
+            return BigInteger.valueOf(1);
+        } else { // bigIntParam = bigIntParam * factorialNum(paraNum - 1)
+            return bigIntParam = bigIntParam.multiply(factorialNum(paraNum - 1));
+        }
     }
 
 }
